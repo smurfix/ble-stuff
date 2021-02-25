86457cbcde4be71f1730ab35dbfa6f9ca5ee7741
Matthias Urlichs <matthias@urlichs.de>
2021-02-25 20:38:35 +0100

    now with database storage

--- beurer.py
+++ beurer.py
@@ -9,9 +9,35 @@ from bleak.exc import BleakDisconnectError
 from functools import partial
 from datetime import datetime
 from dataclasses import dataclass
+from trio_mysql import connect as mysql_connect
+import asyncclick as click
 
 import logging
 logging.basicConfig(level=logging.INFO)
+
+#
+# Database support
+SQL = """
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `u_name` (`name`)
+);
+
+CREATE TABLE `blood_pressure` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` int(11) NOT NULL,
+  `time` datetime NOT NULL,
+  `systole` float DEFAULT NULL,
+  `diastole` float DEFAULT NULL,
+  `arterial` float DEFAULT NULL,
+  `bpm` float DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_time` (`user`,`time`),
+  CONSTRAINT `blood_pressure_user` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE CASCADE
+);
+"""
 
 @dataclass(init=False)
 class Measurement:
@@ -61,44 +87,88 @@ class Measurement:
         val = v & 0x7FF
         return val * (10**exp) * sign
 
+lock = False
 
+async def dev_main(addr,adap, tg, user, once, params):
 
-async def dev_main(addr,adap, sc):
+    async def locker():
+        global lock
+        lock = True
+        with anyio.open_cancel_scope(shield=True):
+            await anyio.sleep(30)
+            lock = False
+    #await tg.spawn(locker)
+
     try:
-        async with BleakClient(addr, adapter=adap) as client:
-            svcs = await client.get_services()
-            bp = svcs["00001810-0000-1000-8000-00805f9b34fb"]
-            char = bp.get_characteristic("00002a35-0000-1000-8000-00805f9b34fb")
+        async with mysql_connect(**params) as conn:
+            async with BleakClient(addr, adapter=adap) as client:
+                svcs = await client.get_services()
+                bp = svcs["00001810-0000-1000-8000-00805f9b34fb"]
+                char = bp.get_characteristic("00002a35-0000-1000-8000-00805f9b34fb")
 
-            print("READ")
-            with anyio.move_on_after(5):
-                async with client.notification(char) as q:
-                    #d = await client.read_gatt_char(char)
-                    #print("GOT",d)
-                    async for d in q:
-                        m = Measurement.decode(d, True)  # Beurer BM85 codes the pulse with wrong byte order
-                        print(m)
+                print("READ")
+                with anyio.move_on_after(5):
+                    async with client.notification(char) as q:
+                        #d = await client.read_gatt_char(char)
+                        #print("GOT",d)
+                        async for d in q:
+                            m = Measurement.decode(d, True)  # Beurer BM85 codes the pulse with wrong byte order
+                            async with conn.transaction(), conn.cursor() as curs:
+                                await curs.execute("select id from blood_pressure where user=%s and `time`=%s", (user,m.time))
+                                r = await curs.fetchone()
+                                print("Q",user,m.time,r)
+                                if r is not None:
+                                    continue
+                                await curs.execute("insert into blood_pressure set user=%s, time=%s, systole=%s,diastole=%s,arterial=%s,bpm=%s", [user,m.time, m.systole,m.diastole,m.arterial,m.pulse])
+
+                                print("W",curs.lastrowid)
+                            print(m)
 
             # done
     except BleakDisconnectError:
         pass
     print("DONE")
-    sc.cancel()
+    if once:
+        tg.cancel_scope.cancel()
 
-def detection_callback(scanner, tg, device, advertisement_data):
+def detection_callback(scanner, tg, user, once, params, device, advertisement_data):
     print(device.address, "RSSI:", device.rssi, advertisement_data)
-    if device.name.startswith("Beurer "):
-        tg.spawn(dev_main,device, scanner._adapter, tg.cancel_scope)
+    if lock:
+        return
+    for uuid in device.details['props'].get('UUIDs',()):
+        if uuid.lower() == "00001810-0000-1000-8000-00805f9b34fb":
+            tg.spawn(dev_main, device, scanner._adapter, tg, user, once, params)
+            break
 
-async def main():
-    scanner = BleakScanner(adapter="hci1")
+@click.async_backend("trio")
+@click.command()
+@click.option("-i","--interface","intf", type=str, help="Bluetooth interface to listen to", default="hci0")
+@click.option("-h","--host", type=str, help="Database host to connect to", default="localhost")
+@click.option("-P","--port", type=int, help="Database port to connect to", default=3306)
+@click.option("-u","--user", type=str, help="Database user", default="test")
+@click.option("-p","--pass", "password", type=str, prompt=True, hide_input=True, help="Database password")
+@click.option("-d","--database", type=str, help="Database to use", default="health")
+@click.option("-o","--one-shot", "once", is_flags=True, help="stop after successful transmission")
+@click.argument("name", type=str, nargs=1)
+async def main(name, intf, once, **params):
+    async with mysql_connect(**params) as conn, conn.transaction():
+        async with conn.cursor() as curs:
+            await curs.execute("select id from user where name=%s", (name,))
+            res = await curs.fetchone()
+            if res is None:
+                await curs.execute("insert into user set name=%s", (name,))
+                user = curs.lastrowid
+            else:
+                user = res[0]
+
+    scanner = BleakScanner(adapter=intf)
     async with anyio.create_task_group() as tg:
-        scanner.register_detection_callback(partial(detection_callback,scanner,tg))
+        scanner.register_detection_callback(partial(detection_callback,scanner,tg,user, once, params))
 
         async with scanner:
             #await anyio.sleep(2)
             #print(await scanner.get_discovered_devices())
             await anyio.sleep(99999)
 
-anyio.run(main, backend="trio")
+main()
 
